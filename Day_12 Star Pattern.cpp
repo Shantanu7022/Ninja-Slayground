83bfc40b2e624502f0796a0ab71c49083c607cf1
Shantanu Gayen <123929397+Shantanu7022@users.noreply.github.com>
2023-09-01 23:45:08 +0530

    update repo

--- Day_12 Star Pattern.cpp
+++ Day_12 Star Pattern.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <bits/stdc++.h> 
 
 void printPattern(int n) {
@@ -12,19 +11,3 @@ void printPattern(int n) {
 		}
 		cout<<endl;
 	}
-=======
-#include <bits/stdc++.h> 
-
-void printPattern(int n) {
-	// Write your code here.
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n-i-1; j++){
-			cout<<" ";
-		}
-		for(int k = 0; k < 2*i+1; k++){
-			cout<<"*";
-		}
-		cout<<endl;
-	}
->>>>>>> 36e45de5b9f205565fee0c8c4d79511b64c07b13
-}
--- Day_13 Fahrenheit to Celsius.cpp
+++ Day_13 Fahrenheit to Celsius.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <bits/stdc++.h> 
 vector<vector<int>> fahrenheitToCelsius(int s, int e, int w)
 {
@@ -10,16 +9,4 @@ vector<vector<int>> fahrenheitToCelsius(int s, int e, int w)
 	}
 	return ans;
 }
-=======
-#include <bits/stdc++.h> 
-vector<vector<int>> fahrenheitToCelsius(int s, int e, int w)
-{
-	//type your code here
-	vector<vector<int>> ans;
-	for(int i = s; i<=e; i = i+w){
-		int j = (i - 32)*5/9;
-		ans.push_back({i,j});
-	}
-	return ans;
-}
->>>>>>> 36e45de5b9f205565fee0c8c4d79511b64c07b13
+
